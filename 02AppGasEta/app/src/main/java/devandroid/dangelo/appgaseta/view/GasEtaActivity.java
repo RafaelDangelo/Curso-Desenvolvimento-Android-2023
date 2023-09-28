7282633747ff3da3fd16e2d332214104120fdf98
RafaelDangelo <rafaelrda@yahoo.com.br>
2023-09-28 11:21:35 -0300

    App GasEta

---
+++ 02AppGasEta/app/src/main/java/devandroid/dangelo/appgaseta/view/GasEtaActivity.java
@@ -0,0 +1,19 @@
+package devandroid.dangelo.appgaseta.view;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import devandroid.dangelo.appgaseta.R;
+
+public class GasEtaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_gaseta);
+    }
+}
